92d51de2f938c6b95c014d4d3183fda9d1e960d4
SamJHudson01 <samjhudson@gmail.com>
2023-08-17 10:56:07 +0100

    feat: set up get and post requests for items

---
+++ src/app/api/items/route.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server"
+import { getItems, addItem } from "../../../models/shoppingListItemDAO"
+import { ShoppingListItem } from "../../../interfaces/ShoppingListItem";
+
+type NewShoppingListItem = Omit<ShoppingListItem, "id" | "completedat">
+
+export const GET = async (request) => {
+    const allItems = await getItems()
+
+    return new NextResponse(JSON.stringify(allItems))
+}
+
+export const POST = async (request) => {
+    const { name: itemName } = await request.json() // Read the name from the request body
+    const newItem: NewShoppingListItem = {
+        name: itemName,
+        quantity: 1, 
+        createdat: new Date(),
+        updatedat: new Date(),
+    };
+
+    const addedItem = await addItem(newItem)
+
+    return new NextResponse(JSON.stringify(addedItem))
+}
