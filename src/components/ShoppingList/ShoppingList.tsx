e00ab30882921dd35bbf57607b24528a57785008
SamJHudson01 <samjhudson@gmail.com>
2023-08-17 10:56:48 +0100

    feat: add item form

---
+++ src/components/ShoppingList/ShoppingList.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import AddItemForm from '../AddItemForm/AddItemForm'
+
+
+
+function ShoppingList() {
+  return (
+    <main className='shopping-list'>
+    
+
+        <AddItemForm />
+
+    </main>
+  )
+}
+
+export default ShoppingList
