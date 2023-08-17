857866293f8b2f3495e8c2e40a507bfb35c7a255
SamJHudson01 <samjhudson@gmail.com>
2023-08-17 11:33:39 +0100

    feat: adds basic GET feature to populate list

--- src/components/ShoppingList/ShoppingList.tsx
+++ src/components/ShoppingList/ShoppingList.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react'
+'use client'
+
+import React, { useState, useEffect } from 'react'
 import AddItemForm from '../AddItemForm/AddItemForm'
 import './ShoppingList.css'
 import ShoppingListItem from '../ShoppingListItem/ShoppingListItem'
@@ -6,25 +8,44 @@ import ShoppingListItem from '../ShoppingListItem/ShoppingListItem'
 
 
 function ShoppingList() {
-   const [items, setItems] = useState([])
-   
+  const [items, setItems] = useState([])
 
+  const fetchItems = async () => {
+    try {
+      const response = await fetch('/api/items');
+      const data = await response.json();
+      setItems(data);
+      console.log(data);
+    } catch (error) {
+      console.error('An error occurred while fetching the items:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchItems();
+  }, [items]); 
 
   return (
     <main className='shopping-list'>
 
 
       <AddItemForm />
-  
+
 
       <div className="shopping-list__item-container">
-        <ShoppingListItem name='Apples'
-          quantity={5}
-          createdat={new Date()}
-          completedat={new Date()}
-          updatedat={new Date()}
-          id={1} />
+        {items.map(item => (
+          <ShoppingListItem
+            key={item.id} // This is the added key prop
+            name={item.name}
+            quantity={item.quantity}
+            createdat={new Date(item.createdat)}
+            completedat={new Date(item.completedat)}
+            updatedat={new Date(item.updatedat)}
+            id={item.id}
+          />
+        ))}
       </div>
+
 
     </main>
   )
