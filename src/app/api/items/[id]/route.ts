34f843c58b8c396df1bdbe1d36efc55671710993
SamJHudson01 <samjhudson@gmail.com>
2023-08-22 20:54:37 +0100

    feat: updates routes to add PUT method

--- src/app/api/items/[id]/route.ts
+++ src/app/api/items/[id]/route.ts
@@ -1,15 +1,35 @@
-import { deleteItem } from '../../../../models/shoppingListItemDAO'; // Adjust the path to your deleteItem function
-import { NextResponse } from 'next/server';
+import {
+  deleteItem,
+  updateItemCompletion,
+} from "../../../../models/shoppingListItemDAO"; // Adjust the path to your deleteItem function
+import { NextResponse } from "next/server";
 
-export async function DELETE(request: Request, { params }: { params: { id: string } }) {
-    console.log('trying to delete stuff and stuff')
-    const id = params.id;
-    try {
-      await deleteItem(Number(id));
-      return new Response(null, { status: 204 });
-    } catch (error) {
-      console.error('Error deleting item:', error);
-      return new Response('Failed to delete item', { status: 500 });
-    }
+export async function DELETE(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  console.log("trying to delete stuff and stuff");
+  const id = params.id;
+  try {
+    await deleteItem(Number(id));
+    return new Response(null, { status: 204 });
+  } catch (error) {
+    console.error("Error deleting item:", error);
+    return new Response("Failed to delete item", { status: 500 });
   }
-  
+}
+
+export async function PUT(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  console.log("trying to update stuff and stuff");
+  const id = Number(params.id);
+  try {
+    const updatedItem = await updateItemCompletion(id);
+    return new Response(JSON.stringify(updatedItem), { status: 200 });
+  } catch (error) {
+    console.error("Error updating item completion:", error);
+    return new Response("Failed to update item completion", { status: 500 });
+  }
+}
