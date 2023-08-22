6718aa0b35ea3512c0983a04bbee33f31eaa0279
SamJHudson01 <samjhudson@gmail.com>
2023-08-22 20:54:37 +0100

    feat: updates routes to add PUT method

--- src/app/api/items/route.ts
+++ src/app/api/items/route.ts
@@ -1,26 +1,35 @@
-import { NextResponse } from "next/server"
-import { getItems, addItem, deleteItem } from "../../../models/shoppingListItemDAO"
+import { NextResponse } from "next/server";
+import {
+  getItems,
+  addItem,
+  deleteItem,
+  updateItemCompletion,
+} from "../../../models/shoppingListItemDAO";
 import { ShoppingListItem } from "../../../interfaces/ShoppingListItemInterface";
 
-type NewShoppingListItem = Omit<ShoppingListItem, "id" | "completedat">
+type NewShoppingListItem = Omit<ShoppingListItem, "id" | "completedat">;
 
 export const GET = async () => {
-    const allItems = await getItems()
-    return new NextResponse(JSON.stringify(allItems))
-}
+  const allItems = await getItems();
+  return new NextResponse(JSON.stringify(allItems));
+};
 
 export const POST = async (request) => {
-    const { name: itemName } = await request.json()
-    const newItem: NewShoppingListItem = {
-        name: itemName,
-        quantity: 1,
-        createdat: new Date(),
-        updatedat: new Date(),
-    };
+  const { name: itemName, quantity: itemQuantity } = await request.json();
+  const newItem: NewShoppingListItem = {
+    name: itemName,
+    quantity: itemQuantity,
+    createdat: new Date(),
+    updatedat: new Date(),
+  };
 
-    const addedItem = await addItem(newItem)
+  const addedItem = await addItem(newItem);
 
-    return new NextResponse(JSON.stringify(addedItem))
-}
+  return new NextResponse(JSON.stringify(addedItem));
+};
 
-
+export const PUT = async (request) => {
+  const { id } = await request.json();
+  const updatedItem = await updateItemCompletion(id);
+  return new NextResponse(JSON.stringify(updatedItem));
+};
